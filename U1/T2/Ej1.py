c7c9f1b46bdf2cf9facd8a343fb796a46c661a50
DavidCesarAliseda <alum.dfernandeza.2020@iesalixar.org>
2021-10-14 21:24:56 +0200

    lista

---
+++ U1/T2/Ej1.py
@@ -0,0 +1,12 @@
+""" Ejercicio 1:
+Invertir una lista dada
+
+Dado:
+lista = [100, 200, 300, 400, 500]
+
+Resultado esperado:
+[500, 400, 300, 200, 100] """
+
+lista = [100, 200, 300, 400, 500]
+listaInvertida = lista[::-1]
+print(listaInvertida)
---
+++ U1/T2/Ej2.py
@@ -0,0 +1,16 @@
+""" Ejercicio 2:
+Concatenar dos listas por índice
+
+Dado:
+lista1 = ["M", "nom", "e", "Che"]
+lista2 = ["i", "bre", "s", "ma"]
+
+Resultado esperado:
+['Mi nombre es Chema'] """
+
+lista1 = ["M", "nom", "e", "Che", "ajajajaja"]
+lista2 = ["i", "bre", "s", "ma"]
+lista3 = zip(lista1, lista2)
+
+
+print(list(lista3))
