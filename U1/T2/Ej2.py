ddce507ec46ee637e18f034244954f9fbf49424f
DavidCesarAliseda <alum.dfernandeza.2020@iesalixar.org>
2021-10-19 20:06:39 +0200

    Listas

--- U1/T2/Ej2.py
+++ U1/T2/Ej2.py
@@ -11,6 +11,15 @@ Resultado esperado:
 lista1 = ["M", "nom", "e", "Che", "ajajajaja"]
 lista2 = ["i", "bre", "s", "ma"]
 lista3 = zip(lista1, lista2)
+cadena = ""
+if(len(lista1) < len(lista2)):
+    corta = 1
+else:
+    corta = 2
 
+""" for i in range(len(lista3)):
+    for j in range(len(lista3[i])):
+        cadena = cadena+lista3[i][j]
+    cadena+" " """
 
-print(list(lista3))
+print(len(lista3))
